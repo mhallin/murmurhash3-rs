96105bb7cb8e26e05a5c33b7551c1b00fc854333
mhallin <mhallin@gmail.com>
2015-03-29 15:27:05 +0200

    Clean up minor syntax changes

--- src/mmh3_128.rs
+++ src/mmh3_128.rs
@@ -157,11 +157,11 @@ mod test {
 
     fn run_bench(b: &mut Bencher, size: u64) {
         let mut data: Vec<u8> = FromIterator::from_iter((0..size).map(|_| 0u8));
-        rand::thread_rng().fill_bytes(&mut data[..]);
+        rand::thread_rng().fill_bytes(&mut data);
 
         b.bytes = size;
         b.iter(|| {
-            black_box(murmurhash3_x64_128(&data[..], 0));
+            black_box(murmurhash3_x64_128(&data, 0));
         });
     }
 
